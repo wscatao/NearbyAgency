b91540886036a6831bd6074baa096b6e0c40b962
Willy Catão <50681413+wscatao@users.noreply.github.com>
2024-11-27 19:08:08 -0300

    feat: inicializa projeto Spring com configuração básica

---
+++ src/main/java/br/com/santander/AgenciaProximaAPI/AgenciaProximaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.santander.AgenciaProximaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaProximaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaProximaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/santander/AgenciaProximaAPI/AgenciaProximaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.santander.AgenciaProximaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaProximaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
