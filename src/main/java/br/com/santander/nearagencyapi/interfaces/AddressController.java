2c81a0f9ade18f8dea2cbb9f85d03b548a5f91ee
Willy Catão <50681413+wscatao@users.noreply.github.com>
2024-12-01 00:29:58 -0300

    chore: Criar exceptions de validação

--- src/main/java/br/com/santander/nearagencyapi/interfaces/AddressController.java
+++ src/main/java/br/com/santander/nearagencyapi/interfaces/AddressController.java
@@ -2,6 +2,8 @@ package br.com.santander.nearagencyapi.interfaces;
 
 import br.com.santander.nearagencyapi.domain.Address;
 import br.com.santander.nearagencyapi.domain.gateway.ZipCodeClientGateway;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +20,11 @@ public class AddressController {
     }
 
     @GetMapping
-    public Address getAddress(@RequestParam(value = "zipcode") String zipcode) {
+    public Address getAddress(
+            @RequestParam(value = "zipcode")
+            @NotNull(message = "zipcode is required")
+            @Size(min = 8, max = 8, message = "zipcode must have 8 characters")
+            String zipcode) {
         return zipCodeClientGateway.getAddress(zipcode);
     }
 }
--- src/main/java/br/com/santander/nearagencyapi/interfaces/exceptions/ControllersExceptionHandlers.java
+++ src/main/java/br/com/santander/nearagencyapi/interfaces/exceptions/ControllersExceptionHandlers.java
@@ -2,14 +2,41 @@ package br.com.santander.nearagencyapi.interfaces.exceptions;
 
 import br.com.santander.nearagencyapi.domain.exception.NearAgencyException;
 import org.springframework.http.ProblemDetail;
+import org.springframework.validation.method.MethodValidationException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllersExceptionHandlers {
 
+    /**
+     * Reminder for problem detail structure:
+     * status
+     * title
+     * detail
+     * property
+     */
+
     @ExceptionHandler(NearAgencyException.class)
     public ProblemDetail handleNearAgencyException(NearAgencyException ex) {
         return ex.toProblemDetail();
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MissingServletRequestParameterException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(400);
+        problemDetail.setTitle("Required parameter is missing");
+        problemDetail.setDetail(ex.getBody().getDetail());
+        problemDetail.setProperty("parameter-name", ex.getParameterName());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MethodValidationException.class)
+    public ProblemDetail handleMethodValidationException(MethodValidationException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(400);
+        problemDetail.setTitle("Invalid request parameters");
+        problemDetail.setDetail("There is invalid fields on the request");
+        return problemDetail;
     }
 }
